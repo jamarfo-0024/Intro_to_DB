8daff991f760e706a1f6295a544532438290b60b
Jude Asante Marfo <judeasantemarfo@gmail.com>
2025-12-14 20:33:34 +0000

    task_2

--- task_2.sql
+++ task_2.sql
@@ -2,13 +2,13 @@
 USE alx_book_store;
 
 -- AUTHORS TABLE
-CREATE TABLE IF NOT EXISTS authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
 -- BOOKS TABLE
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS books (
 );
 
 -- CUSTOMERS TABLE
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS customers (
 );
 
 -- ORDERS TABLE
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS orders (
 );
 
 -- ORDER DETAILS TABLE
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
